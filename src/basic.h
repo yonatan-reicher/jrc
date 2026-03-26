7304cdcfc86597c2f941900386b5350de276b080
Jonathan Reicher <yony252525@gmail.com>
2026-03-26 22:20:05 +0200

    add SYS, EXPECT and EXPECT_ERRNO

--- src/basic.h
+++ src/basic.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 /** Mark a variable as unused */
 #define UNUSED(X) (void)(X)
@@ -27,3 +28,20 @@
         fprintf(stderr, __VA_ARGS__); \
         fprintf(stderr, "\n"); \
     } while (0)
+
+/** Do an expression and panic if it returns false. */
+#define EXPECT(E, ...) \
+    do { \
+        if (!(E)) PANIC(__VA_ARGS__); \
+    } while (0)
+
+/** Do an expression and panic if it returns false, but also print the error
+ * from the error number. */
+#define EXPECT_ERRNO(E) \
+    do { \
+        if (!(E)) \
+            PANIC("'" #E "' failed - %s (errno %d)", strerror(errno), errno); \
+    } while (0)
+
+/** Make a system call and check for -1, print based on the error number. */
+#define SYS(E) EXPECT_ERRNO((E) != -1)
