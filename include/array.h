193761d01995d88db16f2525017ea3d0216e5c27
Jonathan Reicher <yony252525@gmail.com>
2026-05-02 18:41:52 +0300

    update array.*

--- include/array.h
+++ include/array.h
@@ -1,34 +1,66 @@
 // =============================================================================
-//      Array.h
+//                                    Array.h
 // =============================================================================
-// This header files defines macros and functions for working with arrays in a
-// more modern way. Start with defining an array with `DEFINE_ARRAY(T)`. This
-// defines the `Array(T)` type for you.
+// This header file defines macros and functions for working with arrays in a
+// more modern way. Array types can be defined with `DECLARE_ARRAY(T, Name)`
+// (e.g. `DECLARE_ARRAY(char, CharArray)`). `T` must be a mutable type. You can
+// then use the `Name` type. It has fields `.ptr`, `.len` and `.cap`.
 //
-// ---- In this module ---------------------------------------------------------
-// 
-// `DEFINE_ARRAY(T)` - Define the array of type T. This will make a compilation
-// error if it had already been defined for the type, so use an define guard or
-// place all your invocations next to one another.
-//
-// `Array(T)` - The type name of the array of the given type.
-//
-// `array.len` - Every array has this field.
+// Use `array_empty()` to get a new empty array. You may need to cast it first
+// to the correct type (e.g. `(CharArray) array_empty()`). Use `array_push` to
+// add elements to the array. Finally, call `array_free` when you are done.
 // -----------------------------------------------------------------------------
 
 #pragma once
 #include <stddef.h>
 
-/// `Array(T)` is the type of arrays of `T`.
-#define Array(T) _Array_##T
-
-/// Define the array of `T`.
-#define DEFINE_ARRAY(T) \
-    typedef struct Array(T) { size_t len; T *ptr; } Array(T)
+#define DECLARE_ARRAY(T, Name)                                                 \
+    typedef struct {                                                           \
+        T* ptr;                                                                \
+        size_t len;                                                            \
+        size_t cap;                                                            \
+    } Name
 
 /// Return a new empty array of the given type.
-#define array_empty(T) ((Array(T)) { .len = 0, .ptr = NULL })
+#define array_empty() { .len = 0, .cap = 0, .ptr = NULL }
 
-// #define array_add(A, X)
-//     do {
-//         
+#define array_push(ARR, ELEM)                                                  \
+    do {                                                                       \
+        /* Avoid calling `realloc` when capacity is 0. */                      \
+        if ((ARR)->cap == 0) {                                                 \
+            (ARR)->cap = 4;                                                    \
+            (ARR)->ptr = malloc((ARR)->cap * sizeof((ARR)->ptr[0]));           \
+        } else if ((ARR)->len == (ARR)->cap) {                                 \
+            (ARR)->cap *= 2;                                                   \
+            (ARR)->ptr =                                                       \
+                realloc((ARR)->ptr, (ARR)->cap * sizeof((ARR)->ptr[0]));       \
+        }                                                                      \
+        (ARR)->ptr[(ARR)->len++] = (ELEM);                                     \
+    } while (0)
+
+#define array_free(ARR)                                                        \
+    do {                                                                       \
+        free((ARR)->ptr);                                                      \
+        (ARR)->ptr = NULL;                                                     \
+        (ARR)->len = 0;                                                        \
+    } while (0)
+
+/// Empty the array without freeing the memory.
+#define array_clear(ARR)                                                       \
+    do {                                                                       \
+        (ARR)->len = 0;                                                        \
+    } while (0)
+
+#define array_get(ARR, IDX) ((ARR)->ptr[(IDX)])
+
+#define array_pop(ARR)                                                         \
+    do {                                                                       \
+        if ((ARR)->len == 0) PANIC("cannot pop from empty array");             \
+        (ARR)->len--;                                                          \
+    } while (0)
+
+#define array_first(ARR)                                                       \
+    ((ARR)->len == 0                                                           \
+         ? (PANIC("cannot get first element of empty array"), (ARR)->ptr[0])   \
+         : (ARR)->ptr[0])
+#define array_last(ARR) ((ARR)->ptr[(ARR)->len - 1])
--- test/array.c
+++ test/array.c
@@ -1,11 +1,41 @@
 #include "array.h"
 #include "basic.h"
 
-DEFINE_ARRAY(char);
+DECLARE_ARRAY(char, CharArray);
+
+void test_empty_array(void) {
+    CharArray arr = array_empty();
+    EXPECT(arr.ptr == NULL, "Expected empty array to have NULL pointer");
+    EXPECT(arr.len == 0, "Expected empty array to have length 0");
+    EXPECT(arr.cap == 0, "Expected empty array to have capacity 0");
+}
+
+void test_pushing_and_popping(void) {
+    CharArray arr = array_empty();
+    for (char c = 'a'; c <= 'z'; c++) {
+        array_push(&arr, c);
+        EXPECT(
+            arr.len == (size_t)(c - 'a' + 1),
+            "Expected length to be correct after push"
+        );
+    }
+    for (char c = 'z'; c >= 'a'; c--) {
+        EXPECT(
+            arr.ptr[arr.len - 1] == c,
+            "Expected last element to be correct after push"
+        );
+        array_pop(&arr);
+        EXPECT(
+            arr.len == (size_t)(c - 'a'),
+            "Expected length to be correct after pop"
+        );
+    }
+    array_free(&arr);
+    EXPECT(arr.ptr == NULL, "Expected array to be empty after free");
+}
 
 int main(void) {
-    Array(char) chars = array_empty(char);
-    Array(char) chars1 = array_empty(char);
-    if (chars.len != chars1.len) PANIC("aaa");
+    test_empty_array();
+    test_pushing_and_popping();
     return 0;
 }
