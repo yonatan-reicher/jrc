0f13e17bbf0b2b850a2d1c3c58f42f6ca085f578
Jonathan Reicher <yony252525@gmail.com>
2026-05-14 13:15:07 +0300

    formatting

--- src/str.c
+++ src/str.c
@@ -4,24 +4,18 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool str_starts_with(const char *str, const char *prefix)
-{
-    while (prefix[0] != 0)
-    {
-        if (str[0] != prefix[0])
-            return false;
+bool str_starts_with(const char* str, const char* prefix) {
+    while (prefix[0] != 0) {
+        if (str[0] != prefix[0]) return false;
         str++;
         prefix++;
     }
     return true;
 }
 
-void str_remove_newline(char *str)
-{
-    for (; *str != 0; str++)
-    {
-        if (*str == '\n')
-        {
+void str_remove_newline(char* str) {
+    for (; *str != 0; str++) {
+        if (*str == '\n') {
             *str = 0;
             return;
         }
@@ -29,7 +23,7 @@ void str_remove_newline(char *str)
 }
 
 void* malloc(size_t s);
-char* str_clone(const char *s) {
+char* str_clone(const char* s) {
     size_t len = strlen(s);
     char* ret = malloc(len + 1);
     if (ret == NULL) return NULL;
