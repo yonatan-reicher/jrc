ff77d8c4df21c13066e42db468288224a7658682
Jonathan Reicher <yony252525@gmail.com>
2026-05-14 13:13:48 +0300

    formatting

--- include/str.h
+++ include/str.h
@@ -3,13 +3,13 @@
 #include <string.h>
 
 /// Does the first c-string begin with the second?
-bool str_starts_with(const char *str, const char *prefix);
+bool str_starts_with(const char* str, const char* prefix);
 /// If the last character of a c-string is a newline, removes it.
-void str_remove_newline(char *str);
+void str_remove_newline(char* str);
 /// Shorthand for `strcmp(..) == 0`
 #define str_eq(A, B) (strcmp(A, B) == 0)
 /// Duplicate the string. Must be freed!
-char* str_clone(const char *str);
+char* str_clone(const char* str);
 /// Takes a c-string, owned, and returns it as a character array. The string
 /// becomes owned by the array.
 struct CharArray str_to_array(char* str);
