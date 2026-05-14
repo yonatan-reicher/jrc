8863182e58ba507735c6c54acdb59fa67de38cdf
Jonathan Reicher <yony252525@gmail.com>
2026-05-14 13:16:03 +0300

    formatting

--- test/lexer.c
+++ test/lexer.c
@@ -2,27 +2,33 @@
 #include "basic.h"
 
 bool token_eq(const Token* a, const Token* b) {
-    return a->kind == b->kind
-        && a->span.start.index == b->span.start.index
-        && a->span.start.row == b->span.start.row
-        && a->span.start.col == b->span.start.col
-        && a->span.end.index == b->span.end.index
-        && a->span.end.row == b->span.end.row
-        && a->span.end.col == b->span.end.col;
+    return a->kind == b->kind && a->span.start.index == b->span.start.index &&
+           a->span.start.row == b->span.start.row &&
+           a->span.start.col == b->span.start.col &&
+           a->span.end.index == b->span.end.index &&
+           a->span.end.row == b->span.end.row &&
+           a->span.end.col == b->span.end.col;
 }
 
-#define EXPECT_TOKEN_EQ(A, B) \
-    do { \
-        if (!token_eq(&(A), &(B))) { \
-            PANIC( \
-                "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT " instead.", \
-                TOKEN_PRINTF((B)), \
-                TOKEN_PRINTF((A)) \
-            ); \
-        } \
+#define EXPECT_TOKEN_EQ(A, B)                                                  \
+    do {                                                                       \
+        if (!token_eq(&(A), &(B))) {                                           \
+            PANIC(                                                             \
+                "Expected " TOKEN_PRINTF_FORMAT                                \
+                " but got " TOKEN_PRINTF_FORMAT " instead.",                   \
+                TOKEN_PRINTF((B)),                                             \
+                TOKEN_PRINTF((A))                                              \
+            );                                                                 \
+        }                                                                      \
     } while (0)
 
-Token new_token(TokenKind kind, size_t start_index, TextRow start_row, TextCol start_col, int len) {
+Token new_token(
+    TokenKind kind,
+    size_t start_index,
+    TextRow start_row,
+    TextCol start_col,
+    int len
+) {
     return (Token) {
         .kind = kind,
         .span = {
@@ -40,8 +46,8 @@ Token new_token(TokenKind kind, size_t start_index, TextRow start_row, TextCol s
     };
 }
 
-#define NEW_TOKEN(KIND, START_INDEX, START_ROW, START_COL, LEN) \
-    new_token(TOKEN_KIND_ ## KIND, START_INDEX, START_ROW, START_COL, LEN)
+#define NEW_TOKEN(KIND, START_INDEX, START_ROW, START_COL, LEN)                \
+    new_token(TOKEN_KIND_##KIND, START_INDEX, START_ROW, START_COL, LEN)
 
 void test_eof(void) {
     Lexer lexer = lexer_new("");
@@ -49,7 +55,8 @@ void test_eof(void) {
     const Token expected = NEW_TOKEN(EOF, 0, 1, 1, 0);
     if (!token_eq(&token, &expected)) {
         PANIC(
-            "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT " instead.",
+            "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT
+            " instead.",
             TOKEN_PRINTF(expected),
             TOKEN_PRINTF(token)
         );
@@ -62,7 +69,8 @@ void test_single_int(void) {
     const Token expected = NEW_TOKEN(INT, 0, 1, 1, 3);
     if (!token_eq(&token, &expected)) {
         PANIC(
-            "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT " instead.",
+            "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT
+            " instead.",
             TOKEN_PRINTF(expected),
             TOKEN_PRINTF(token)
         );
@@ -75,7 +83,8 @@ void test_ints_with_whitespace(void) {
     const Token expected1 = NEW_TOKEN(INT, 5, 2, 3, 3);
     if (!token_eq(&token1, &expected1)) {
         PANIC(
-            "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT " instead.",
+            "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT
+            " instead.",
             TOKEN_PRINTF(expected1),
             TOKEN_PRINTF(token1)
         );
@@ -84,7 +93,8 @@ void test_ints_with_whitespace(void) {
     const Token expected2 = NEW_TOKEN(INT, 13, 3, 3, 3);
     if (!token_eq(&token2, &expected2)) {
         PANIC(
-            "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT " instead.",
+            "Expected " TOKEN_PRINTF_FORMAT " but got " TOKEN_PRINTF_FORMAT
+            " instead.",
             TOKEN_PRINTF(expected2),
             TOKEN_PRINTF(token2)
         );
@@ -109,12 +119,9 @@ void test_symbols(void) {
     const char* text = "+-*/()";
     Lexer lexer = lexer_new(text);
     Token expected[] = {
-        NEW_TOKEN(PLUS, 0, 1, 1, 1),
-        NEW_TOKEN(MINUS, 1, 1, 2, 1),
-        NEW_TOKEN(STAR, 2, 1, 3, 1),
-        NEW_TOKEN(SLASH, 3, 1, 4, 1),
-        NEW_TOKEN(LPAREN, 4, 1, 5, 1),
-        NEW_TOKEN(RPAREN, 5, 1, 6, 1),
+        NEW_TOKEN(PLUS, 0, 1, 1, 1),   NEW_TOKEN(MINUS, 1, 1, 2, 1),
+        NEW_TOKEN(STAR, 2, 1, 3, 1),   NEW_TOKEN(SLASH, 3, 1, 4, 1),
+        NEW_TOKEN(LPAREN, 4, 1, 5, 1), NEW_TOKEN(RPAREN, 5, 1, 6, 1),
         NEW_TOKEN(EOF, 6, 1, 7, 0),
     };
     for (size_t i = 0; i < ARRAY_LEN(expected); i++) {
