30a0c45571b8f5a9d115ddcd3e67635ecd470246
Jonathan Reicher <yony252525@gmail.com>
2026-08-20 12:56:19 +0300

    update whatever

--- src/whatever.c
+++ src/whatever.c
@@ -23,23 +23,27 @@ DECLARE_SLICE(const MLModel, ConstMLModelSlice);
 int main(void) {
     srand((unsigned int)time(NULL));
     // 3 inputs, 2 outputs
-    NEW_MAT(weights, 2, 3, {});
-    NEW_MAT(biases, 2, 1, {});
+    NEW_MAT(weights1, 2, 3, {});
+    NEW_MAT(biases1, 2, 1, {});
+    NEW_MAT(weights2, 2, 2, {});
+    NEW_MAT(biases2, 2, 1, {});
     NEW_MAT(out, 2, 1, {});
     NEW_MAT(output_error, 2, 1, {});
-    DenseLayer _p = dense_layer_new(weights, biases);
-    dense_layer_randomize(&_p, -1.0f, 1.0f);
+    DenseLayer _p1 = dense_layer_new(weights1, biases1);
+    dense_layer_randomize(&_p1, -1.0f, 1.0f);
+    DenseLayer _p2 = dense_layer_new(weights2, biases2);
+    dense_layer_randomize(&_p2, -1.0f, 1.0f);
     SequentialLayer l = sequential_layer_new(
-        slice(VoidPtrConstSlice, { &_p, NULL }),
-        slice(ConstMLModelSlice, { dense_layer_ml_model(), sigmoid_ml_model() })
+        slice(VoidPtrConstSlice, { &_p1, NULL, &_p2 }),
+        slice(ConstMLModelSlice, { dense_layer_ml_model(), sigmoid_ml_model(), dense_layer_ml_model() })
     );
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 5000; i++) {
         for (size_t j = 0; j < dataset_len; j++) {
             MatView input = { dataset[j].input, { 3, 1 } };
             MatView target = { dataset[j].target, { 2, 1 } };
             sequential_layer_forward(&l, &input, &out);
             mat_sub(&target, &out, &output_error);
-            sequential_layer_train(&l, &input, &output_error, 0.05f);
+            sequential_layer_train(&l, &input, &output_error, 0.013f);
         }
         float error_sum = 0;
         for (size_t j = 0; j < output_error.shape.n_rows; j++) {
