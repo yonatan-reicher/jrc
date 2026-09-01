83e2fee60551d2174547efb18c14b509d65a0798
Jonathan Reicher <yony252525@gmail.com>
2026-09-01 15:50:21 +0300

    give MLModel's field's their own typedefs

--- include/ml_model.h
+++ include/ml_model.h
@@ -1,20 +1,26 @@
 #pragma once
 #include "mat.h"
 
+typedef void MLModelForward(const void* self, const MatView* inp, MatView* out);
+typedef void MLModelBackward(
+    const void* self,
+    const MatView* inp,
+    const MatView* out_err,
+    MatView* inp_err
+);
+typedef void MLModelTrain(
+    void* self, const MatView* inp, const MatView* out_err, float lr
+);
+typedef bool MLModelSupportsInpShape(const void* self, MatShape inp_shape);
+typedef MatShape MLModelOutShape(const void* self, MatShape inp_shape);
+
 /// An interface for machine learning models. Instances support mapping inputs
 /// to outputs, deriving input errors from output errors, and training on output
 /// errors.
 typedef struct MLModel {
-    void (*forward)(const void* self, const MatView* inp, MatView* out);
-    void (*backward)(
-        const void* self,
-        const MatView* inp,
-        const MatView* out_err,
-        MatView* inp_err
-    );
-    void (*train)(
-        void* self, const MatView* inp, const MatView* out_err, float lr
-    );
-    bool (*supports_inp_shape)(const void* self, MatShape inp_shape);
-    MatShape (*out_shape)(const void* self, MatShape inp_shape);
+    MLModelForward* forward;
+    MLModelBackward* backward;
+    MLModelTrain* train;
+    MLModelSupportsInpShape* supports_inp_shape;
+    MLModelOutShape* out_shape;
 } MLModel;
